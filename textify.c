54017c9317d6ec4a0bb4bd55a6a66b6c61a353ff
mihir-thakkar-ai <mihirthakkar98@gmail.com>
2020-11-02 16:52:18 +0530

    Added all the work till 2Nov2020

--- textify.c
+++ textify.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include<termios.h>
 #include<stdlib.h>
+#include<ctype.h>
+#include<stdio.h>
 
 struct termios orig_termios; 
 void disableRawMode() {
@@ -11,7 +13,8 @@ void enableRawMode() {
 	atexit(disableRawMode);
 	
 	struct termios raw=orig_termios;
-	raw.c_lflag &= ~(ECHO);
+	raw.c_iflag &= ~(IXON | ICRNL);
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // TCSAFLUSH: Change attributes when output has drained; also flush pending input.
 
@@ -22,7 +25,21 @@ void enableRawMode() {
 int main() {
     enableRawMode();
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c!='q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+
+		if (iscntrl(c))
+		{
+			printf("%d\n", c);
+			
+		}
+		else {
+
+			printf("%d ('%c')\n", c, c);
+		}
+
+
+
+	}
 	return 0;
 
 }
