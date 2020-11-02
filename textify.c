d1364eee780a027317b6192c7c5d46c08371d2aa
mihir-thakkar-ai <mihirthakkar98@gmail.com>
2020-11-02 17:41:53 +0530

    Added all the work till 2Nov2020

--- textify.c
+++ textify.c
@@ -1,42 +1,63 @@
+
+/*** includes ***/
+
 #include <unistd.h>
 #include<termios.h>
 #include<stdlib.h>
 #include<ctype.h>
+#include<errno.h>
 #include<stdio.h>
 
-struct termios orig_termios; 
+
+/*** data ***/
+struct termios orig_termios;
+
+
+/*** terminal ***/
+void die(const char* s) {
+
+	perror(s);
+	exit(1);
+}
 void disableRawMode() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+	// tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 void enableRawMode() {
-	tcgetattr(STDIN_FILENO, &orig_termios);
+
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+	// tcgetattr(STDIN_FILENO, &orig_termios);
 	atexit(disableRawMode);
 	
 	struct termios raw=orig_termios;
-	raw.c_iflag &= ~(IXON | ICRNL);
+	raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
 	raw.c_lflag &= ~(ECHO | ICANON | ISIG);
-
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // TCSAFLUSH: Change attributes when output has drained; also flush pending input.
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)==-1) die("tcsetattr"); // TCSAFLUSH: Change attributes when output has drained; also flush pending input.
 
 }
 
-
+/*** init ***/
 
 int main() {
     enableRawMode();
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-
+	while ( 1 ) {
+		char c = '\0';
+		if(read(STDIN_FILENO, &c, 1)==-1 && errno!=EAGAIN) die("read");
 		if (iscntrl(c))
 		{
-			printf("%d\n", c);
+			printf("%d\r\n", c);
 			
 		}
 		else {
 
-			printf("%d ('%c')\n", c, c);
+			printf("%d ('%c')\r\n", c, c);
 		}
-
+		if (c == 'q')
+			break;
 
 
 	}
